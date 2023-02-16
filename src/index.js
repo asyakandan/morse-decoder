2f63098a8b4a6b57b4c1f0f2294f08c0dcb49b3d
asyakandan <asyakandan@gmail.com>
2023-02-16 17:43:20 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+	const text = expr.split('**********');
+	const res = [];
 
+	text.map(el => {
+		let word = '';
+
+		for (let i = 0; i < el.length; i += 10) {
+			let morseCode = '';
+
+			for (let j = i; j < i + 10; j += 2) {
+				if (el[j] != 0) {
+					if (el[j + 1] == 0) {
+						morseCode += '.';
+					} else if (el[j + 1] == 1) {
+						morseCode += '-';
+					}
+				}
+			}
+
+			word += MORSE_TABLE[morseCode];
+		}
+
+		res.push(word);
+	});
+
+	return res.join(' ');
 }
 
 module.exports = {
